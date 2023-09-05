55ca4ca4c6b9206c5ae8be8900262fc7cb89627c
Adam Bieszk <adambieszk6@gmail.com>
2023-09-05 22:33:59 +0200

    Add revive as Alive Tainted Lazarus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { ModCallback } from "isaac-typescript-definitions";
+import {
+  CollectibleType,
+  ModCallback,
+  PlayerType,
+} from "isaac-typescript-definitions";
 
 const MOD_NAME = "alt-revives";
 
@@ -9,12 +13,37 @@ export function main(): void {
   const mod = RegisterMod(MOD_NAME, 1);
 
   // Register a callback function that corresponds to when a new player is initialized.
-  mod.AddCallback(ModCallback.POST_PLAYER_INIT, postPlayerInit);
+  mod.AddCallback(ModCallback.POST_UPDATE, revive);
 
   // Print a message to the "log.txt" file.
   Isaac.DebugString(`${MOD_NAME} initialized.`);
 }
 
-function postPlayerInit() {
-  Isaac.DebugString("Callback fired: POST_PLAYER_INIT");
+function revive() {
+  const player = Isaac.GetPlayer();
+  if (
+    player.IsDead() &&
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+    player.GetPlayerType() >= 21
+  ) {
+    if (player.HasCollectible(CollectibleType.LAZARUS_RAGS)) {
+      reviveAsTLazarus(player);
+    } else {
+      player.Revive();
+    }
+  }
+}
+
+function reviveAsTLazarus(player: EntityPlayer) {
+  Isaac.DebugString("Player dead, reviving as Alive Tainted Lazarus...");
+  print("Player dead, reviving as Alive Tainted Lazarus...");
+  player.ChangePlayerType(PlayerType.LAZARUS_B);
+  player.AddMaxHearts(-24, false);
+  player.AddSoulHearts(-24);
+  player.AddBlackHearts(-24);
+  player.Revive();
+  player.SetPocketActiveItem(CollectibleType.FLIP);
+  player.AnimateCollectible(CollectibleType.LAZARUS_RAGS);
+  player.RemoveCollectible(CollectibleType.LAZARUS_RAGS);
+  player.AddHearts(6);
 }
